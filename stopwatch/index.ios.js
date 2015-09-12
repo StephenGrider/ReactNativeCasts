fd12ab008884144365a4f3eda5c612673f9e72a5
stephen grider <ste.grider@gmail.com>
2015-09-12 14:48:19 -0700

    scaffold stopwatch app

---
+++ stopwatch/index.ios.js
@@ -0,0 +1,14 @@
+var React = require('react-native');
+var {
+  Text,
+  View,
+  AppRegistry
+} = React;
+
+var StopWatch = React.createClass({
+  render: function() {
+
+  }
+});
+
+AppRegistry.registerComponent('stopwatch', () => StopWatch);
