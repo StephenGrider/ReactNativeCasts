4bb8d214eef67784374f54a05af784c0be906821
stephen grider <ste.grider@gmail.com>
2015-09-12 14:26:10 -0700

    added styling

--- weekdays/src/day-item.js
+++ weekdays/src/day-item.js
@@ -1,22 +1,35 @@
 // Import some code we need
 var React = require('react-native');
 var Text = React.Text;
-var StyleSheet = React.StyleSheet;
 
 // Create our component
 var DayItem = React.createClass({
   render: function() {
-    return <Text style={styles.day}>
+    return <Text style={this.style()}>
       {this.props.day}
     </Text>
-  }
-});
-
-// Style our component
-var styles = StyleSheet.create({
-  day: {
-    fontSize: 18,
-    color: '#0000FF'
+  },
+  style: function() {
+    return {
+      color: this.color(),
+      fontWeight: this.fontWeight(),
+      fontSize: this.fontSize(),
+      lineHeight: this.lineHeight()
+    }
+  },
+  color: function() {
+    var opacity = 1 / this.props.daysUntil;
+    return 'rgba(0, 0, 0, '+ opacity + ')';
+  },
+  fontWeight: function() {
+    var weight = 7 - this.props.daysUntil;
+    return weight * 100;
+  },
+  fontSize: function() {
+    return 60 - 6 * this.props.daysUntil;
+  },
+  lineHeight: function() {
+    return 70 - 4 * this.props.daysUntil;
   }
 });
 
