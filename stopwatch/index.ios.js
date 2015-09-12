f4955d12a4a4a41680cc8362d34d9e13fd14a9d5
stephen grider <ste.grider@gmail.com>
2015-09-12 14:58:44 -0700

    move jsx to helper methods

--- stopwatch/index.ios.js
+++ stopwatch/index.ios.js
@@ -7,7 +7,27 @@ var {
 
 var StopWatch = React.createClass({
   render: function() {
-
+    return <View>
+      <Text>
+        00:00.00
+      </Text>
+      {this.startStopButton()}
+      {this.lapButton()}
+    </View>
+  },
+  startStopButton: function() {
+    return <View>
+      <Text>
+        Start
+      </Text>
+    </View>
+  },
+  lapButton: function() {
+    return <View>
+      <Text>
+        Lap
+      </Text>
+    </View>
   }
 });
 
