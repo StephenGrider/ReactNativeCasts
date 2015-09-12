413bb249f8fd53da89a7b5ec217e074fdee621c4
stephen grider <ste.grider@gmail.com>
2015-09-12 15:40:52 -0700

    positioned buttons and timer

--- stopwatch/index.ios.js
+++ stopwatch/index.ios.js
@@ -10,12 +10,12 @@ var StopWatch = React.createClass({
   render: function() {
     return <View style={styles.container}>
       <View style={[styles.header, this.border('yellow')]}>
-        <View style={this.border('red')}>
+        <View style={[styles.timerWrapper, this.border('red')]}>
           <Text>
             00:00.00
           </Text>
         </View>
-        <View style={this.border('green')}>
+        <View style={[styles.buttonWrapper, this.border('green')]}>
           {this.startStopButton()}
           {this.lapButton()}
         </View>
@@ -60,6 +60,17 @@ var styles = StyleSheet.create({
   },
   footer: { // Blue
     flex: 1
+  },
+  timerWrapper: { // Red
+    flex: 5, // takes up 5/8ths of the available space
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonWrapper: { // Green
+    flex: 3, // takes up 3/8ths of the available space
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   }
 });
 
