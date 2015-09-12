239821c9c10b40fbba09d799a375e9b56e821566
stephen grider <ste.grider@gmail.com>
2015-09-12 12:23:43 -0700

    Added styling to view tag

--- weekdays/index.ios.js
+++ weekdays/index.ios.js
@@ -3,15 +3,25 @@ var React = require('react-native');
 var AppRegistry = React.AppRegistry;
 var Text = React.Text;
 var View = React.View;
+var StyleSheet = React.StyleSheet;
 
 // Create a react component
 var Weekdays = React.createClass({
   render: function() {
-    return <View>
+    return <View style={styles.container}>
       <Text>
         Days of the week:
       </Text>
     </View>
+  }
+});
+
+// Style the React component
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 
