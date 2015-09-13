4e6dc27fc5b78bff8024587f9a930fbd776d309c
stephen grider <ste.grider@gmail.com>
2015-09-12 18:13:18 -0700

    timer can be stopped

--- stopwatch/index.ios.js
+++ stopwatch/index.ios.js
@@ -11,7 +11,8 @@ var {
 var StopWatch = React.createClass({
   getInitialState: function() {
     return {
-      timeElapsed: null
+      timeElapsed: null,
+      running: false
     }
   },
   render: function() {
@@ -36,13 +37,15 @@ var StopWatch = React.createClass({
     </View>
   },
   startStopButton: function() {
+    var style = this.state.running ? styles.stopButton : styles.startButton;
+
     return <TouchableHighlight
       underlayColor="gray"
       onPress={this.handleStartPress}
-      style={[styles.button, styles.startButton]}
+      style={[styles.button, style]}
       >
       <Text>
-        Start
+        {this.state.running ? 'Stop' : 'Start'}
       </Text>
     </TouchableHighlight>
   },
@@ -54,11 +57,18 @@ var StopWatch = React.createClass({
     </View>
   },
   handleStartPress: function() {
+    if(this.state.running){
+      clearInterval(this.interval);
+      this.setState({running: false});
+      return
+    }
+
     var startTime = new Date();
 
-    setInterval(() => {
+    this.interval = setInterval(() => {
       this.setState({
-        timeElapsed: new Date() - startTime
+        timeElapsed: new Date() - startTime,
+        running: true
       });
     }, 30);
   }
@@ -99,6 +109,9 @@ var styles = StyleSheet.create({
   },
   startButton: {
     borderColor: '#00CC00'
+  },
+  stopButton: {
+    borderColor: '#CC0000'
   }
 });
 
