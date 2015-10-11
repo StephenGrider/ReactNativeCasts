5717ca7afad0841c8b852d42ecfe405db3c7b9c4
stephen grider <ste.grider@gmail.com>
2015-10-11 16:04:36 -0700

    added api module

--- weather/src/api.js
+++ weather/src/api.js
@@ -1,5 +1,21 @@
 var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=f557b20727184231a597c710c8be3106';
 
+var kelvinToF = function(kelvin) {
+  return Math.round((kelvin - 273.15) * 1.8 + 32) + ' ˚F'
+};
+
 module.exports = function(latitude, longitude) {
   var url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;
+
+  fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json){
+      return {
+        city: json.city,
+        temperature: kelvinToF(json.main.temp),
+        description: json.weather[0].description
+      }
+    });
 }
