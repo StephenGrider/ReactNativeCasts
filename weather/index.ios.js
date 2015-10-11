ee52150af09ed3f4e58cf0fb1fd6e3b5b7ea1579
stephen grider <ste.grider@gmail.com>
2015-10-11 15:26:41 -0700

    pins place with state

--- weather/index.ios.js
+++ weather/index.ios.js
@@ -7,20 +7,28 @@ var {
 } = React;
 
 var Weather = React.createClass({
+  getInitialState: function() {
+    return {
+      pin: {
+        latitude: 0,
+        longitude: 0
+      }
+    };
+  },
   render: function() {
-    var pins = [{
-      latitude: 37,
-      longitude: -95
-    }];
-
     return <MapView
-      annotations={pins}
+      annotations={[this.state.pin]}
       onRegionChangeComplete={this.onRegionChangeComplete}
       style={styles.map}>
     </MapView>
   },
   onRegionChangeComplete: function(region) {
-
+    this.setState({
+      pin: {
+        longitude: region.longitude,
+        latitude: region.latitude
+      }
+    });
   }
 });
 
