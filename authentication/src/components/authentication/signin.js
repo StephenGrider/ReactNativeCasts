1be677179a0f1a905062b0ce5adf16c81e0633ee
stephen grider <ste.grider@gmail.com>
2015-11-21 14:24:51 -0800

    Added username and password fields

--- authentication/src/components/authentication/signin.js
+++ authentication/src/components/authentication/signin.js
@@ -11,6 +11,10 @@ module.exports = React.createClass({
     return (
       <View style={styles.container}>
         <Text>Sign In</Text>
+        <Text style={styles.label}>Username:</Text>
+        <TextInput style={styles.input} />
+        <Text style={styles.label}>Password:</Text>
+        <TextInput secureTextEntry={true} style={styles.input} />
       </View>
     );
   }
@@ -21,5 +25,18 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  input: {
+    padding: 4,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 5,
+    width: 200,
+    alignSelf: 'center'
+  },
+  label: {
+    fontSize: 18
   }
 });
