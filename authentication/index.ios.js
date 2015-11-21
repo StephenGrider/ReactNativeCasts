a1b6f60f1f4d02afb0edfc5282f2b023a6580ed3
stephen grider <ste.grider@gmail.com>
2015-11-21 13:55:15 -0800

    register main with root index files

---
+++ authentication/index.ios.js
@@ -0,0 +1,6 @@
+var React = require('react-native');
+var { AppRegistry } = React;
+
+var Main = require('./src/main');
+
+AppRegistry.registerComponent('authentication', () => Main);
--- authentication/src/main.js
+++ authentication/src/main.js
@@ -9,13 +9,13 @@ module.exports = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text>I am on both iOS and Andrdoid!</Text>
+        <Text>I am on both iOS and Android!</Text>
       </View>
-    )
+    );
   }
 });
 
-var styles = Stylesheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
