39f79febcb3be0bf20ac14e57ce1372469f5c204
stephen grider <ste.grider@gmail.com>
2015-11-21 15:12:45 -0800

    Added parse init code

--- authentication/src/main.js
+++ authentication/src/main.js
@@ -8,6 +8,9 @@ var {
 var Signin = require('./components/authentication/signin');
 
 module.exports = React.createClass({
+  componentWillMount: function() {
+    Parse.initialize("epHqnJOxMHEKGloGBgZCpn6SIKHdxX5q97a5iVMj", "uZXxWDuXj9msgTzSQAtPKYmWnBWooKGPsU5k7Bp4");
+  },
   render: function() {
     return (
       <View style={styles.container}>
