3b8039e67ab19f39dad0ca924a6787b32e3253a8
stephen grider <ste.grider@gmail.com>
2015-11-21 16:33:30 -0800

    implement navigator

--- authentication/src/main.js
+++ authentication/src/main.js
@@ -7,16 +7,24 @@ var {
 var Parse = require('parse/react-native');
 var Signin = require('./components/authentication/signin');
 
+var ROUTES = {
+  signin: Signin
+};
+
 module.exports = React.createClass({
   componentWillMount: function() {
     Parse.initialize("epHqnJOxMHEKGloGBgZCpn6SIKHdxX5q97a5iVMj", "uZXxWDuXj9msgTzSQAtPKYmWnBWooKGPsU5k7Bp4");
   },
+  renderScene: function(route, navigator) {
+    var Component = ROUTES[route.name]; // ROUTES['signin'] => Signin
+    return <Component />;
+  },
   render: function() {
     return (
       <Navigator
-        style={style.container}
-        initialRoute={}
-        renderScene={}
+        style={styles.container}
+        initialRoute={{name: 'signin'}}
+        renderScene={this.renderScene}
         configureScene={() => { return Navigator.SceneConfigs.FloatFromRight; }}
         />
     );
