2860f5368adff8d27320d3ef94881ca8a5a1daf8
stephen grider <ste.grider@gmail.com>
2015-11-21 17:41:04 -0800

    display current username

--- authentication/src/components/tweets/tweets.js
+++ authentication/src/components/tweets/tweets.js
@@ -5,11 +5,28 @@ var {
   Text
 } = React;
 
+var Parse = require('parse/react-native');
+
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      user: null
+    };
+  },
+  componentWillMount: function() {
+    Parse.User.currentAsync()
+      .then((user) => { this.setState({user: user}); })
+  },
   render: function() {
+    if (!this.state.user) {
+      return <Text>Loading...</Text>;
+    }
+
+    var username = this.state.user.get('username');
+
     return (
       <View style={styles.container}>
-        <Text>Welcome back!</Text>
+        <Text>Welcome back, {username}!</Text>
       </View>
     );
   }
