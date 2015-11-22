b9f37c89ea8c14de788e9fd60e521dee595cf03e
stephen grider <ste.grider@gmail.com>
2015-11-21 16:55:17 -0800

    fleshed out signup form

--- authentication/src/components/authentication/signup.js
+++ authentication/src/components/authentication/signup.js
@@ -2,16 +2,49 @@ var React = require('react-native');
 var {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  TextInput
 } = React;
 
+var Button = require('../common/button');
+
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      username: '',
+      password: '',
+      passwordConfirmation: ''
+    };
+  },
   render: function() {
     return (
       <View style={styles.container}>
-        <Text>You can sign up here!</Text>
+        <Text>Sign Up</Text>
+
+        <Text style={styles.label}>Username:</Text>
+        <TextInput
+          value={this.state.username}
+          onChangeText={(text) => this.setState({username: text})}
+          style={styles.input} />
+
+        <Text style={styles.label}>Password:</Text>
+        <TextInput
+          value={this.state.password}
+          onChangeText={(text) => this.setState({password: text})}
+          style={styles.input} />
+
+        <Text style={styles.label}>Confirm Password:</Text>
+        <TextInput
+          value={this.state.passwordConfirmation}
+          onChangeText={(text) => this.setState({passwordConfirmation: text})}
+          style={styles.input} />
+
+        <Button text={'I have an account...'} onPress={this.onSigninPress} />
       </View>
     );
+  },
+  onSigninPress: function() {
+    this.props.navigator.pop();
   }
 });
 
@@ -21,5 +54,18 @@ var styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white'
+  },
+  label: {
+    fontSize: 18
+  },
+  input: {
+    padding: 4,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 5,
+    width: 200,
+    alignSelf: 'center'
   }
 });
